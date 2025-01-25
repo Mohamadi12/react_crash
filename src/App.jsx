c90f2fb5bad1cc55134bdbff808da43bccf158a4
Nana Mohamadi <adilnana78@gmail.com>
2025-01-25 22:06:36 +0100

    Tailwind CSS setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </div>
+    </div>
   )
 }
 
