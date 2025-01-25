031400b4c916e73144769841633675905886b13c
Nana Mohamadi <adilnana78@gmail.com>
2025-01-25 21:10:04 +0100

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+const Card = () => {
+  return (
+    <div>
+      <h2>Card Component</h2>
+    </div>
+  )
+}
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h2>Functional Arrow Component</h2>
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
